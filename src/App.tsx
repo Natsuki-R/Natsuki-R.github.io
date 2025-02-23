0b029be1e91ad16630f4c56caf5c2a3ec33ef303
Natsuko-r <shenjiaojun@gmail.com>
2025-02-23 11:08:20 +0900

    Initial commit for my-portfolio

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import viteLogo from '/favicon.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Hello Natsuki</h1>
+    </>
+  )
+}
+
+export default App
