f1cb623ee56469b51c75cd7c16165a377a3ea405
Natsuko-R <shenjiaojun@gmail.com>
2025-02-26 12:56:42 +0900

    customize radio style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from "./components/Header"
-import Home from "./components/Home"
-import Blog from "./components/Blog"
-import Bookmark from "./components/Bookmark"
-import About from "./components/About"
-import Projects from "./components/Projects"
-import Contact from "./components/Contact"
-import Music from "./components/Music"
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Blog from "./components/Blog";
+import Bookmark from "./components/Bookmark";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Music from "./components/Music";
+import "./App.css";
 
 function App() {
   return (
@@ -32,14 +32,12 @@ function App() {
           add social links:
           ins, x, spotify, github, qiita
 
-          fix radio button's responsive bug in Music Page 
-
           unify the font-size css for responsive, add scope
            */}
         </footer>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,39 @@
-import React from "react"
-import { Link } from 'react-router-dom';
-import "./Header.css"
-import profile from '../assets/profile.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import profile from "../assets/profile.png";
 
 const Header: React.FC = () => {
-    return (
-        <header className="header">
-            <div>
-                <a href="/">
-                    <img src={profile} className="logo" alt="Vite logo" />
-                </a>
-            </div>
-            <nav className="nav">
-                <div><Link to="/">Home</Link></div>
-                <div><Link to="/about">About</Link></div>
-                <div><Link to="/blog">Blog</Link></div>
-                <div><Link to="/music">Music</Link></div>
-                <div><Link to="/bookmark">Bookmark</Link></div>
-                <div><Link to="/projects">Projects</Link></div>
-                <div><Link to="/contact">Contact</Link></div>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <div>
+        <a href="/">
+          <img src={profile} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <nav className="nav">
+        {/* <div><Link to="/">Home</Link></div> */}
+        <div>
+          <Link to="/about">About</Link>
+        </div>
+        <div>
+          <Link to="/blog">Blog</Link>
+        </div>
+        <div>
+          <Link to="/music">Music</Link>
+        </div>
+        <div>
+          <Link to="/bookmark">Bookmark</Link>
+        </div>
+        <div>
+          <Link to="/projects">Projects</Link>
+        </div>
+        <div>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
