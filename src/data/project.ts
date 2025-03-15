5f19ad40a51b976ac8e7c5c8235a6ed110a77ac6
Natsuko-r <shenjiaojun@gmail.com>
2025-03-15 18:32:34 +0900

    add new project

--- src/data/project.ts
+++ src/data/project.ts
@@ -1,15 +1,15 @@
 import project1 from "../assets/project1.png";
 import project2 from "../assets/project2.png";
 import project3 from "../assets/project3.png";
-import project4 from "../assets/project2.jpg";
+import project4 from "../assets/project4.png";
 import { project } from "../types/project";
 
 export const PROJECTS: project[] = [
   {
-    title: "GLTF Viewer",
-    description: "A GLTF 3d model viewer built with Three.js and Next.js.",
-    url: "https://demo-gltf-viewer.vercel.app/",
-    imageUrl: project1,
+    title: "Digital-Archive",
+    description: "paintings, C4D models, diy... all those stupid sh*t that I made before :)",
+    url: "https://digital-archive-natsuki.vercel.app/",
+    imageUrl: project4,
   },
   {
     title: "Legacy Personal Site",
@@ -19,15 +19,15 @@ export const PROJECTS: project[] = [
     imageUrl: project2,
   },
   {
+    title: "GLTF Viewer",
+    description: "A GLTF 3d model viewer built with Three.js and Next.js.",
+    url: "https://demo-gltf-viewer.vercel.app/",
+    imageUrl: project1,
+  },
+  {
     title: "3D Cemetery",
     description: "You can design your own cemetery funeral experience with 3D models.",
     url: "https://3d-cemetery.vercel.app/",
     imageUrl: project3,
-  },
-  {
-    title: "3D Gallery",
-    description: "Coming soon...",
-    url: "",
-    imageUrl: project4,
   },
 ];
